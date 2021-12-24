bf8d26caa5a3669d8a0fc7ff158d84082d4c829b
baohoang94 <hb9445@gmail.com>
2021-12-24 23:27:42 +0700

    Phần 37: Áp Dụng Soft Delete Xóa Sản Phẩm

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('admins/product/index/list.css') }}">
 @endsection
 @section('js')
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{ asset('vendors/sweetAlert2/sweetalert2@11.js') }}"></script>
 <script src="{{ asset('admins/product/index/list.js') }}"></script>
 @endsection
 
