ce6d4d61a1092c3e1fda366faba48100f21797a8
baohoang94 <hb9445@gmail.com>
2022-01-01 21:05:23 +0700

    Phần 91 Phân quyền với gate và policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,9 +25,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('category-list', function ($user) {
-            return $user->checkPermissinAccess(config('permissions.access.list-category'));
-        });
+        Gate::define('category-list', 'App\Policies\CategoryPolicy@view');
+        Gate::define('category-add', 'App\Policies\CategoryPolicy@create');
+        Gate::define('category-edit', 'App\Policies\CategoryPolicy@update');
+        Gate::define('category-delete', 'App\Policies\CategoryPolicy@delete');
+        // Gate::define('category-list', function ($user) {
+        //     return $user->checkPermissinAccess(config('permissions.access.list-category'));
+        // });
         Gate::define('menu-list', function ($user) {
             return $user->checkPermissinAccess(config('permissions.access.list-menu'));
         });
--- routes/web.php
+++ routes/web.php
@@ -25,23 +25,28 @@ Route::prefix('admin')->group(function () {
         ]);
         Route::get('create', [
             'as' => 'categories.create',
-            'uses' => 'CategoryController@create'
+            'uses' => 'CategoryController@create',
+            'middleware' => 'can:category-add'
         ]);
         Route::post('store', [
             'as' => 'categories.store',
-            'uses' => 'CategoryController@store'
+            'uses' => 'CategoryController@store',
+            'middleware' => 'can:category-add'
         ]);
         Route::get('edit/{id}', [
             'as' => 'categories.edit',
-            'uses' => 'CategoryController@edit'
+            'uses' => 'CategoryController@edit',
+            'middleware' => 'can:category-edit'
         ]);
         Route::post('update/{id}', [
             'as' => 'categories.update',
-            'uses' => 'CategoryController@update'
+            'uses' => 'CategoryController@update',
+            'middleware' => 'can:category-edit'
         ]);
         Route::get('delete/{id}', [
             'as' => 'categories.delete',
-            'uses' => 'CategoryController@delete'
+            'uses' => 'CategoryController@delete',
+            'middleware' => 'can:category-delete'
         ]);
     });
     Route::prefix('menus')->group(function () {
