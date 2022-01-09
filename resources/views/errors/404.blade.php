2b12e78c4162e17a5959cffb5050ae498f27c0a4
baohoang94 <hb9445@gmail.com>
2022-01-09 23:09:43 +0700

    them trang 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Trang không tồn tại</title>
+    <link href="{{ asset('eshopper/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('eshopper/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('eshopper/css/prettyPhoto.css') }}" rel="stylesheet">
+    <link href="{{ asset('eshopper/css/price-range.css') }}" rel="stylesheet">
+    <link href="{{ asset('eshopper/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('eshopper/css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<div class="container text-center">
+    <div class="content-404">
+        <img src="{{ asset('eshopper/images/404/404.png') }}" class="img-responsive" alt="404" />
+        <h1><b>OPPS!</b> Trang không tồn tại</h1>
+        <p>Chúng tôi không tìm thấy trang bạn yêu cầu.</p>
+        <h2><a href="{{ route('home') }}">Về trang chủ</a></h2>
+    </div>
+</div>
+<script src="{{ asset('eshopper/js/jquery.js') }}"></script>
+<script src="{{ asset('eshopper/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('eshopper/js/jquery.scrollUp.min.js') }}"></script>
+<script src="{{ asset('eshopper/js/price-range.js') }}"></script>
+<script src="{{ asset('eshopper/js/jquery.prettyPhoto.js') }}"></script>
+<script src="{{ asset('eshopper/js/main.js') }}"></script>
+</body>
+
+</html>
